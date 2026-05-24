d7ed3ac63dc1148faf5b6dae68129305c25c9cc5
Onkarrrrrrr <onkarpathrikar2004@gmail.com>
2026-05-24 19:27:11 +0530

    Fix build_files.sh by adding a virtual environment to ensure Django is installed and removing migrate command

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
